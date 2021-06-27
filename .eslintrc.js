f274a2d7150bd570bba96312ea8b3e69f29716c6
MrTylerjet <MrTylerjet@gmail.com>
2021-06-26 23:39:57 -0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'prettier', 'eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+  },
+};
