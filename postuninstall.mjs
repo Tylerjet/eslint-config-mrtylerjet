d247cdef3e6ce8a5adddd7521e6395c8a242ff6c
MrTylerjet <MrTylerjet@gmail.com>
2021-11-09 19:54:13 -0700

    uninstall seems to run twice on yarn so the second run can't find the files since they are already deleted, uninstall not supported by npm v7 though oh well

--- postuninstall.mjs
+++ postuninstall.mjs
@@ -2,10 +2,10 @@ import { unlink } from 'fs';
 import { join } from 'path';
 
 unlink(join(process.env.INIT_CWD,'.prettierrc'), (err) => {
-  if (err) {console.log('.prettierrc does not exist!')};
+  if (err) {};
   // Completed
 });
 unlink(join(process.env.INIT_CWD,'.eslintrc'), (err) => {
-  if (err) {console.log('.eslintrc does not exist!')};
+  if (err) {};
   // Completed
 });
