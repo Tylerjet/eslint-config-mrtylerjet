3cd8ee0fb94feb242fd3077d924d6b39659636ea
MrTylerjet <MrTylerjet@gmail.com>
2021-11-09 19:33:47 -0700

    didn't need array brackets either

--- postinstall.mjs
+++ postinstall.mjs
@@ -1,9 +1,9 @@
 import { copyFile } from 'fs';
 import { join } from 'path';
 
-copyFile('.eslintrc', join([process.env.INIT_CWD, '.eslintrc']), (err) => {
+copyFile('.eslintrc', join(process.env.INIT_CWD, '.eslintrc'), (err) => {
   if (err) throw err;
 });
-copyFile('.prettierrc', join([process.env.INIT_CWD, '.prettierrc']), (err) => {
+copyFile('.prettierrc', join(process.env.INIT_CWD, '.prettierrc'), (err) => {
   if (err) throw err;
 });
--- postuninstall.mjs
+++ postuninstall.mjs
@@ -1,11 +1,11 @@
 import { unlink } from 'fs';
 import { join } from 'path';
 
-unlink(join([process.env.INIT_CWD,'.prettierrc']), (err) => {
+unlink(join(process.env.INIT_CWD,'.prettierrc'), (err) => {
   if (err) {console.log('.prettierrc does not exist!')};
   // Completed
 });
-unlink(join([process.env.INIT_CWD,'.eslintrc']), (err) => {
+unlink(join(process.env.INIT_CWD,'.eslintrc'), (err) => {
   if (err) {console.log('.eslintrc does not exist!')};
   // Completed
 });
