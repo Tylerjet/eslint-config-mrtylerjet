d27edecec447f380d40951e7685c530daecea093
MrTylerjet <MrTylerjet@gmail.com>
2021-11-09 19:08:23 -0700

    gotta find a good way get the directory without doing ../../

--- postuninstall.js
+++ postuninstall.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
-
-fs.unlink('.prettierrc', (err) => {
-  if (err) {console.log('.prettierrc does not exist!')};
-  // Completed
-});
-fs.unlink('.eslintrc', (err) => {
-  if (err) {console.log('.eslintrc does not exist!')}
-})
+console.log(process)
+// fs.unlink('.prettierrc', (err) => {
+//   if (err) {console.log('.prettierrc does not exist!')};
+//   // Completed
+// });
+// fs.unlink('.eslintrc', (err) => {
+//   if (err) {console.log('.eslintrc does not exist!')};
+//   // Completed
+// });`
