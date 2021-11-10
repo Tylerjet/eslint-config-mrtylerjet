48a249d191ecdd850beadae458da029815f1fdc3
MrTylerjet <MrTylerjet@gmail.com>
2021-11-09 19:17:41 -0700

    lets see

--- postuninstall.js
+++ postuninstall.js
@@ -1,10 +1,11 @@
-const fs = require('fs');
-console.log(process)
-// fs.unlink('.prettierrc', (err) => {
-//   if (err) {console.log('.prettierrc does not exist!')};
-//   // Completed
-// });
-// fs.unlink('.eslintrc', (err) => {
-//   if (err) {console.log('.eslintrc does not exist!')};
-//   // Completed
-// });`
+import unlink from 'fs';
+import join from 'path';
+
+unlink(join([process.env.INIT_CWD,'.prettierrc']), (err) => {
+  if (err) {console.log('.prettierrc does not exist!')};
+  // Completed
+});
+unlink(join([process.env.INIT_CWD,'.eslintrc']), (err) => {
+  if (err) {console.log('.eslintrc does not exist!')};
+  // Completed
+});
